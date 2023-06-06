dc8d4bd3bb8a3829411e49288fccf3daa39b68e2
nkwaaaa <nkwaaaa@gmail.com>
2023-06-06 09:13:36 -0300

    add cart context

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Footer } from "./components/Footer";
 import Header from "./components/Header";
 import Products from "./components/Products";
 import { IS_DEVELOPMENT } from "./config";
+import { CartProvider } from "./context/cart";
 import useFilters from "./hooks/useFilters";
 import { products as initialProducts } from "./mocks/products.json";
 
@@ -14,11 +15,11 @@ export default function App() {
 	const filteredProducts = filterProducts(products);
 
 	return (
-		<>
+		<CartProvider>
 			<Header />
 			<Cart />
 			<Products products={filteredProducts} />
 			{IS_DEVELOPMENT && <Footer />}
-		</>
+		</CartProvider>
 	);
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
+import useCart from "../hooks/useCart";
 import useFilters from "../hooks/useFilters";
 import "./Footer.css";
 
 export function Footer() {
 	const { filters } = useFilters();
+	const { cart } = useCart();
 	return (
 		<footer className="footer">
 			<h4>Prueba técnica de React ⚛️</h4>
 			<h5>Shopping Cart con useContext & useReducer</h5>
 			<h3>Arroyo Matías Hernán</h3>
-			{JSON.stringify(filters, null, 2)}
+			{JSON.stringify(filters, null, 2)} <br />
+			{JSON.stringify(cart, null, 2)}
 		</footer>
 	);
 }
